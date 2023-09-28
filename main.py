c34dc1b87b23e2794d3f50872c3ad9ad1fac45a6
Fluubah <youngsonaaron@gmail.com>
2023-09-28 10:25:11 -0500

    Initial Commit - NO NEW CODE

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class LoginPageApp(App):
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class LoginManager(ScreenManager):
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Question1Screen(Screen):
+    pass
+
+
+LoginPageApp().run()
